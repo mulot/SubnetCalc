0f860a40268fa1deebeec8e4c88c4243248702cf
Julien Mulot <julien.mulot@gmail.com>
2020-11-20 11:59:32 +0100

    Add identifier for UI elements Add a complete UI Unit Test

--- SubnetCalcUITests/SubnetCalcUITests.m
+++ SubnetCalcUITests/SubnetCalcUITests.m
@@ -35,18 +35,32 @@
     
     XCUIElement *subnetcalcWindow = [[XCUIApplication alloc] init].windows[@"SubnetCalc"];
     
-    XCUIElement *iptextfieldcellTextField = subnetcalcWindow.textFields[@"iptextfieldcell"];
+    XCUIElement *iptextfieldcellTextField = subnetcalcWindow.textFields[@"ipaddrfield"];
     [iptextfieldcellTextField typeText:@"10.32.2.52/30\r"];
     XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"subnetbitscombo"] value], @"22");
     XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"maskbitscombo"] value], @"30");
-    XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"subnetbitscombo"] value], @"22");
     XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"subnetmaskcombo"] value], @"255.255.255.252");
     XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"maxsubnetcombo"] value], @"4194304");
     XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"maxhostscombo"] value], @"2");
-    XCTAssert([subnetcalcWindow.staticTexts[@"subnetidtext"] exists]);
     XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"subnetrangetext"] value], @"10.32.2.53 - 10.32.2.54");
     XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"subnetidtext"] value], @"10.32.2.52");
     XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"subnetbroadcasttext"] value], @"10.32.2.55");
+    [subnetcalcWindow.tabs[@"CIDR"] click];
+    XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"cidrmaskbits"] value], @"30");
+    XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"cidrmask"] value], @"255.255.255.252");
+    XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"cidrmaxsubnets"] value], @"4");
+    XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"cidrmaxaddr"] value], @"2");
+    XCTAssertEqualObjects([subnetcalcWindow.comboBoxes[@"cidrmaxsupernets"] value], @"1");
+    XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"cidrnetwork"] value], @"10.32.2.52");
+    XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"cidrrange"] value], @"10.32.2.52 - 10.32.2.55");
+    [subnetcalcWindow.tabs[@"Address"] click];
+    XCTAssertEqualObjects([subnetcalcWindow.popUpButtons[@"addrclasstypecell"] value], @"Class A : 1.0.0.0 - 126.255.255.255");
+    XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"classbitmap"] value], @"nnnnnnnn.ssssssss.ssssssss.sssssshh");
+    XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"binarymap"] value], @"00001010.00100000.00000010.00110100");
+    XCTAssertEqualObjects([subnetcalcWindow.staticTexts[@"hexamap"] value], @"0A.20.02.34");
+    
+
+    //XCTAssert([subnetcalcWindow.popUpButtons[@"addrclasstypecell"] exists]);
     //[subnetcalcWindow/*@START_MENU_TOKEN@*/.tabs[@"Subnets"]/*[[".tabGroups.tabs[@\"Subnets\"]",".tabs[@\"Subnets\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ click];
     [iptextfieldcellTextField typeKey:@"q" modifierFlags:XCUIKeyModifierCommand];
     
