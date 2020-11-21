6f7bfdc6a8b68d5b3928a1163546b6e3e298b9ec
Julien Mulot <julien.mulot@gmail.com>
2020-11-21 16:13:54 +0100

    Enable stop After failure of UI tests

--- SubnetCalcUITests/SubnetCalcUITests.m
+++ SubnetCalcUITests/SubnetCalcUITests.m
@@ -17,7 +17,7 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
 
     // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = YES;
+    self.continueAfterFailure = NO;
 
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
